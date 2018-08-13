32e53b629272bdc4cb69ebae178c779dbea50a38
Nicolas Nollomont <nicolasnollomont@Whatever.lan>
2018-08-13 13:06:13 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import './App.css' ;
+import Searchbox from './Searchbox'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
+      searchfield: ''
     }
   };
 
@@ -17,11 +19,21 @@ class App extends Component {
       this.setState({robots:users})
     })
   };
+
+  onSearchChange = (event) => {
+    this.setState({searchfield:event.target.value})
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+     })
+   
     return (
      <div className='tc'>
        <h1>Robofriends</h1>
-       <Cardlist robots={this.state.robots}/>
+       <Searchbox searchChange={this.onSearchChange} />
+       <Cardlist robots={filteredRobots}/>
      </div>
     );
   }
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 
-const Searchbox = () => {
+const Searchbox = ({searchChange}) => {
 	return (
 		<div className='pa2'>
-		<input type='search' placeholder='Search robots...'></input>
+		<input 
+		className='pa3 ba b--blue bg-lightest-blue' 
+		type='search' 
+		placeholder='Search robots...'
+		onChange={searchChange}></input>
 		</div>
 		);
 };
