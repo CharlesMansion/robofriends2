dacec4dc62da89a9718795a0cdda23f738717ed9
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2019-01-13 22:56:27 +0100

    adding favicons

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -4,11 +4,13 @@ import React from 'react';
 const Searchbox = ({searchChange}) => {
 	return (
 		<div className='pa2'>
-		<input 
-		className='pa3 ba b--blue bg-light-yellow' 
-		type='search' 
-		placeholder='Search robots...'
-		onChange={searchChange}></input>
+			<label for="search">
+			<input 
+			className='pa3 ba b--blue bg-light-yellow' 
+			type='search' 
+			placeholder='Search robots...'
+			onChange={searchChange}></input>
+			</label>
 		</div>
 		);
 };
