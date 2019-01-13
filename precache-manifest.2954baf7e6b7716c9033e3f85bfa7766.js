64bee14974303d785cbcf20b132859921f6e6bbf
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2019-01-13 18:47:10 +0100

    Updates

---
+++ precache-manifest.2954baf7e6b7716c9033e3f85bfa7766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/robofriends2/static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "78469173679fc51fd272",
+    "url": "/robofriends2/static/js/runtime~main.78469173.js"
+  },
+  {
+    "revision": "6c90bf97c161d17273f8",
+    "url": "/robofriends2/static/js/main.6c90bf97.chunk.js"
+  },
+  {
+    "revision": "8489f5319904925894ef",
+    "url": "/robofriends2/static/js/1.8489f531.chunk.js"
+  },
+  {
+    "revision": "6c90bf97c161d17273f8",
+    "url": "/robofriends2/static/css/main.1fec9d8e.chunk.css"
+  },
+  {
+    "revision": "8489f5319904925894ef",
+    "url": "/robofriends2/static/css/1.11793d8c.chunk.css"
+  },
+  {
+    "revision": "84b56d342689cd8e1aec980080b32820",
+    "url": "/robofriends2/index.html"
+  }
+];
