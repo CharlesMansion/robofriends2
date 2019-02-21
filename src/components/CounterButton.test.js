5a7cc1816d3d79c09cb9261303d19261403d741c
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2019-02-21 08:59:25 +0100

    adding files

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -2,4 +2,14 @@ import React from 'react';
 import {shallow} from 'enzyme'
 import CounterButton from './CounterButton';
 
+describe('testing statefulness of CounterButton', () => {
+	const wrapper = shallow(<CounterButton />)
+	const instance = wrapper.instance();
+
+	it('renders correctly', () => {
+		expect(wrapper).toMatchSnapshot();
+		wrapper.find('[id="counter"]').simulate('click');
+		expect(instance.state.count).toEqual(1);
+	})
+})
 
